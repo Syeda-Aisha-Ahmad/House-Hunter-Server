9fcf52ecb04d6e2a10adf3fa10537c2e805bc825
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2023-07-19 11:18:07 +0600

    post add new homes, get new homes

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zu49cyt.mongodb.net/?retryWrites=true&w=majority`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v0q5o0h.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -22,9 +23,25 @@ const client = new MongoClient(uri, {
 });
 async function run() {
     try {
+        const homeCollection = client.db('houseHunter').collection('houseCollection');
+
+
+        // Add New House
+        app.post('/addHouse', async (req, res) => {
+            const query = req.body;
+            const result = await homeCollection.insertOne(query);
+            res.send(result);
+        })
+
+        // Get new house dataS
+        app.get('/addHouse', async (req, res) => {
+            const query = {};
+            const houses = await homeCollection.find(query).toArray();
+            res.send(houses);
+        });
 
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
